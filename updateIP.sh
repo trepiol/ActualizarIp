2165d1e79d803f24e975c558cafaee38d2f34f19
Ezequiel <147086944+trepiol@users.noreply.github.com>
2025-02-09 16:31:10 -0300

    Add files via upload

---
+++ updateIP.sh
@@ -0,0 +1,42 @@
+[ec2-user@ip-10-0-1-148 updateIP]$ cat actualizacionDeIp.sh 
+#!/bin/bash
+#script para actualizar la direccion ip con el dominio de noip
+
+
+#credenciales
+
+#llamo donde estan las variables
+
+source .env
+
+noip_user="$user"
+noip_pass="$pass"
+noip_host="$host"
+user_agent="agent"
+url_noip="https://dynupdate.no-ip.com/nic/update"
+archivoLog="$Log"
+
+#obtener ip de la instancia de aws
+ip=$(curl ifconfig.me)
+
+#generar la solicitud a noip y realizar la actualizacion para la nueva ip
+solicitud=$(curl -s -u "$noip_user:$noip_pass" \
+  -A "$user_agent" \
+  "$url_noip?hostname=$noip_host&myip=$ip")
+
+# registrar los eventos en el archivos de registros
+echo "$(date): $solicitud" >> "$archivoLog"
+
+#interceptar la respuesta
+case "$solicitud" in
+        good*)
+                echo "IP actualizada correctamente"
+                ;;
+        nochg*)
+                echo "La IP $ip ya esta actualizada"
+                ;;
+        *)
+                echo "Error al actualizar la ip: $solicitud"
+                ;;
+esac
+
